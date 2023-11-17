9ceede2451d3f830c9fb9b8c9db8611c0e353fc6
JohnyWander <j.myslinski.pl@gmail.com>
2023-11-17 10:53:03 +0100

    Some messages to user

--- Program.cs
+++ Program.cs
@@ -17,6 +17,13 @@ namespace Win11PrintFIxer
                 WorkKey.SetValue("RpcAuthentication", 0, RegistryValueKind.DWord);
                 WorkKey.SetValue("RpcUseNamedPipeProtocol", 1, RegistryValueKind.DWord);
 
+                Console.WriteLine(@"Successfully added registry key Software\\Policies\\Microsoft\\Windows NT\Printers\RPC\RpcAuthentication, 0 value DWORD");
+                Console.WriteLine(@"Successfully added registty key Software\\\\Policies\\\\Microsoft\\\\Windows NT\\Printers\\RPC\\RpcAuthentication , 1 value DWORD");
+                Console.WriteLine(@"Your network printing should work now, if not feel free to open an issue on https://github.com/JohnyWander/Win11NetworkPrintFix");
+                Console.WriteLine("There are several things more in Win 11 that can make problems with network printing");
+                Console.WriteLine("Press any ley to exit");
+                Console.ReadLine();
+
             } catch (System.Security.SecurityException)
             {
                 Console.WriteLine("Please run this program with administrator rights");
